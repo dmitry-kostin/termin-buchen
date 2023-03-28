e225f2d1ebfd2511cd35ffdbf8ca492cf171c1ed
Jimmy Kaplowitz <jimmy@kaplowitz.org>
2023-03-28 19:28:33 +0200

    Support node-chromedriver configuration

--- src/driver.ts
+++ src/driver.ts
@@ -1,8 +1,10 @@
+import * as chromedriver from 'chromedriver';
 import {Builder, WebDriver} from 'selenium-webdriver';
 import * as chrome from 'selenium-webdriver/chrome';
 import {UserPromptHandler} from 'selenium-webdriver/lib/capabilities';
 
 export const createDriver = async (): Promise<WebDriver> => {
+  const service = new chrome.ServiceBuilder(chromedriver.path);
   const options = new chrome.Options();
   options.addArguments(
     // '--headless',
@@ -11,5 +13,5 @@ export const createDriver = async (): Promise<WebDriver> => {
     '--disable-dev-shm-usage'
   );
   options.setAlertBehavior(UserPromptHandler.ACCEPT);
-  return new Builder().forBrowser('chrome').setChromeOptions(options).build();
+  return new Builder().forBrowser('chrome').setChromeService(service).setChromeOptions(options).build();
 };
